efff3edee8fe8d6916572dcc303e6b1190229704
wax42 <vguerand@student.42.fr>
2018-10-29 12:19:24 +0100

    Update flask_api

--- app/flask_api.py
+++ app/flask_api.py
@@ -5,6 +5,20 @@ app = Flask(__name__)
 def hello():
     return "Hello World from Flask"
 
+
+
+@app.route("/testapic")
+def testapic():
+    return "testapic" 
+
+@app.route("/speechtotext")
+def speech():
+    return "Speech to text"
+
+@app.route("/sentiment")
+def sentiment():
+    return "Some beautiful sentiment"
+
 if __name__ == "__main__":
     # Only for debugging while developing
     app.run(host='0.0.0.0', debug=True, port=80)
